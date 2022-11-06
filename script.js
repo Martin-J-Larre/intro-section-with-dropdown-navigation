00549bfe1bdf31ad00047949eacd097e4f292000
Martin-J-Larre <martinjlarre@gmail.com>
2022-11-05 23:04:37 -0400

    #8 Nav finished/ Main section styles started

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const hamburger = document.querySelector('.nav__hamburger');
+const navOverlay = document.querySelector('.nav__overlay');
 
+hamburger.addEventListener('click',() => {
+    hamburger.classList.toggle('nav__hamburger--open');
+    navOverlay.classList.toggle('nav__overlay--open');
+});
 
-hamburger.addEventListener('click',() => hamburger.classList.toggle('nav__hamburger--open'));
+navOverlay.addEventListener('click', (e) => {
+    e.preventDefault();
+    const currentElement = e.target;
 
+    if (subMenu(currentElement, 'nav__item--parent')) {
+
+        const dropDown = currentElement.parentElement.children[1];
+        
+        if (window.innerWidth < 768) {
+            let height = dropDown.clientHeight == 0 ? dropDown.scrollHeight : 0;
+            dropDown.style.height = `${height}px`
+        } else {
+            
+        }
+    }
+});
+
+const subMenu = (element, nameClass) => { 
+    return element.classList.value.includes(nameClass);
+}
